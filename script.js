07b075da1696b2a092b89ace13806a1a3a8f85c8
Aariyan <aariyan2137@gmail.com>
2026-06-20 21:50:51 +0530

    defining variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello, World!");
+const accountId = 1234;
+let gmailId = "user@gmail.com";
+let accountstate;
+accountMail = "me@gmail.com";
+console.table({accountId, gmailId, accountstate, accountMail});
+
+// var is not used. let and const are instead.
+
+// alert("hello") // doesn't work in node it only works in browser.
