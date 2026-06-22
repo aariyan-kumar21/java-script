e2b819ee27046e16ff7c7b3d6cc01e7ed2e74563
Aariyan <aariyan2137@gmail.com>
2026-06-22 15:32:44 +0530

    new file

---
+++ strings.js
@@ -0,0 +1,10 @@
+const name = "chai";
+const price = 10;
+
+console.log(`${name.toUpperCase()} pilo guys ${price} rupe ki.`);
+console.log(name.charAt(2));
+console.log(name.indexOf('c'));
+
+let naam = "Aariyan";
+console.log(naam.substring(0,3));
+console.log(naam.slice(-7,-4));
